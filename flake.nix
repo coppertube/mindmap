635fda092bd027193bee29ea2e2cce410c911be0
GetPsyched <priyanshu@getpsyched.dev>
2024-06-25 11:43:34 +0400

    feat: init flakebox

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    flakebox.url = "github:rustshop/flakebox";
+    flakebox.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flakebox }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        projectName = "mindmap";
+
+        flakeboxLib = flakebox.lib.${system} {
+          config = {
+            github.ci.buildOutputs = [ ".#ci.${projectName}" ];
+          };
+        };
+
+        buildPaths = [
+          "Cargo.toml"
+          "Cargo.lock"
+          "src"
+        ];
+
+        buildSrc = flakeboxLib.filterSubPaths {
+          root = builtins.path {
+            name = projectName;
+            path = ./.;
+          };
+          paths = buildPaths;
+        };
+
+        multiBuild =
+          (flakeboxLib.craneMultiBuild { }) (craneLib':
+            let
+              craneLib = (craneLib'.overrideArgs {
+                pname = projectName;
+                src = buildSrc;
+                nativeBuildInputs = [ ];
+              });
+            in
+            {
+              ${projectName} = craneLib.buildPackage { };
+            });
+      in
+      {
+        packages.default = multiBuild.${projectName};
+
+        legacyPackages = multiBuild;
+
+        devShells = flakeboxLib.mkShells { };
+      }
+    );
+}

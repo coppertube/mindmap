7d2caad03ad6b1f4981c392f50fe7bf68f48799c
GetPsyched <priyanshu@getpsyched.dev>
2024-07-01 08:24:04 +0400

    feat: boilerplate Task struct

--- src/bin/todo/create.rs
+++ src/bin/todo/create.rs
@@ -1,8 +1,12 @@
 use clap::Parser;
+use mindmap::Task;
 
 #[derive(Parser)]
 pub struct Args {}
 
 pub fn command(_args: &Args) {
-    println!("Task created successfully!");
+    let task = Task {
+        description: String::from("don't slack off"),
+    };
+    println!("Task \"{}\" created successfully!", task.description);
 }

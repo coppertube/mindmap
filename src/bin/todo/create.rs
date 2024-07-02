a08bd66a8dbc893a3daf3eea86704ce4ae34ae1e
GetPsyched <priyanshu@getpsyched.dev>
2024-07-02 12:04:31 +0400

    feat: interactive cli for todo create

--- src/bin/todo/create.rs
+++ src/bin/todo/create.rs
@@ -1,12 +1,31 @@
+use chrono::{Local, Weekday};
 use clap::Parser;
-use mindmap::Task;
+use inquire::{DateSelect, Select};
+use mindmap::{Difficulty, Priority, Task};
 
 #[derive(Parser)]
 pub struct Args {}
 
 pub fn command(_args: &Args) {
     let task = Task {
-        description: String::from("don't slack off"),
+        description: inquire::prompt_text("Description").expect("An error occurred!"),
+        difficulty: Select::new(
+            "Difficulty",
+            vec![Difficulty::Low, Difficulty::Medium, Difficulty::High],
+        )
+        .prompt_skippable()
+        .expect("An error occurred!"),
+        priority: Select::new(
+            "Priority",
+            vec![Priority::Low, Priority::Medium, Priority::High],
+        )
+        .prompt_skippable()
+        .expect("An error occurred!"),
+        deadline: DateSelect::new("Deadline")
+            .with_min_date(Local::now().date_naive())
+            .with_week_start(Weekday::Mon)
+            .prompt_skippable()
+            .expect("An error occurred!"),
     };
     println!("Task \"{}\" created successfully!", task.description);
 }

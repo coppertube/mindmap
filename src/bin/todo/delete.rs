71394ec7e3b6f37c0cfbdd9192ab9c4a66f80053
GetPsyched <priyanshu@getpsyched.dev>
2024-07-02 14:31:39 +0400

    feat: todo delete interface

--- src/bin/todo/delete.rs
+++ src/bin/todo/delete.rs
@@ -1,8 +1,37 @@
 use clap::Parser;
+use inquire::Select;
+use mindmap::Task;
 
 #[derive(Parser)]
 pub struct Args {}
 
 pub fn command(_args: &Args) {
-    println!("Deleted task successfully!")
+    let tasks = [
+        Task {
+            description: String::from("learn rust"),
+            difficulty: None,
+            priority: None,
+            deadline: None,
+        },
+        Task {
+            description: String::from("build mindmap cli"),
+            difficulty: None,
+            priority: None,
+            deadline: None,
+        },
+        Task {
+            description: String::from("build mindmap gui"),
+            difficulty: None,
+            priority: None,
+            deadline: None,
+        },
+    ];
+    let task_description = Select::new(
+        "Select the task to delete:",
+        tasks.iter().map(|task| &task.description).collect(),
+    )
+    .prompt()
+    .expect("An error occurred!");
+
+    println!("Task \"{}\" deleted successfully!", task_description);
 }
